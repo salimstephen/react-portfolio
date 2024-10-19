5bb3102d71e7991752df32068ddd2f3ad9ec9eb9
salimstephen <stephensalim505@gmail.com>
2024-10-20 00:18:34 +0300

    new changes

--- src/content_option.js
+++ src/content_option.js
@@ -1,23 +1,24 @@
-const logotext = "JOHN";
+const logotext = "Stephen salim";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Stephen Salim",
+    description: "I’m a Frontend developer",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Stephen Salim",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
         third: "I develop mobile apps",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "I am a passionate frontend developer skilled in creating dynamic and responsive web applications. I am proficient in modern frameworks React, allowing me to build interactive interfaces that engage users across devices. I thrive in collaborative environments, working closely with designers and backend developers to deliver high-quality web solutions.",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
     title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    aboutme: "As a frontend developer, I am dedicated to crafting engaging and user-friendly web experiences. My journey in web development began with a passion for design and technology, leading me to master HTML, CSS, and JavaScript. I enjoy transforming complex concepts into intuitive interfaces and strive to create seamless interactions that delight users. I have experience with popular frameworks like React and Vue.js, which I leverage to build responsive, high-performance applications. I thrive on collaboration and value feedback, working closely with designers and backend developers to ensure the final product meets both functional and aesthetic goals.",
+
 };
 const worktimeline = [{
         jobtitle: "Designer of week",
@@ -37,11 +38,11 @@ const worktimeline = [{
 ];
 
 const skills = [{
-        name: "Python",
+        name: "HTML",
         value: 90,
     },
     {
-        name: "Djano",
+        name: "CSS",
         value: 85,
     },
     {
@@ -53,22 +54,31 @@ const skills = [{
         value: 60,
     },
     {
-        name: "Jquery",
+        name: " Git ",
         value: 85,
     },
 ];
 
-const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+const services = [
+    {
+        title: "Website Development",
+        description: "Creating responsive and interactive websites using HTML, CSS, and JavaScript.",
     },
     {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "SEO Optimization",
+        description: "Implementing best practices for search engine optimization to improve site visibility.",
     },
     {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Version Control Management",
+        description: "Utilizing tools like Git for version control and collaboration on projects.",
+    },
+    {
+        title: "Performance Optimization",
+        description: "Improving website loading speed and overall performance through code optimization and resource management.",
+    },
+    {
+        title: "Continuous Learning and Adaptation",
+        description: "Staying updated with the latest technologies and trends in web development.",
     },
 ];
 
@@ -136,17 +146,18 @@ const dataportfolio = [{
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "stephensalim505@gmail.com",
+    YOUR_FONE: "+254708055990",
+    description: "I’d love to hear from you! Whether you have a question about my work, want to discuss a project, or just want to connect, feel free to reach out. You can contact me via email or connect with me on social media. I look forward to hearing from you!",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+    YOUR_SERVICE_ID: " contact_service.",
+    YOUR_TEMPLATE_ID: "contact_form",
+    YOUR_USER_ID: "ADO9KTz0f1ohKTQ3V",
 };
 
 const socialprofils = {
+  
     github: "https://github.com",
     facebook: "https://facebook.com",
     linkedin: "https://linkedin.com",
